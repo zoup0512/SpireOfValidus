3ee0d1d558107d3fd26df40686b62861b3401bf3
zoup0512 <zoup0512@gmail.com>
2018-11-15 11:32:32 +0800

    after folk first ci

--- app/src/main/java/com/example/administrator/spireofvalidus/application/MyApplication.java
+++ app/src/main/java/com/example/administrator/spireofvalidus/application/MyApplication.java
@@ -2,17 +2,17 @@ package com.example.administrator.spireofvalidus.application;
 
 import android.app.Application;
 
-import com.example.administratop.spirefvalidus.MyEventBusIndex;
+
+import com.example.myapp.MyEventBusIndex;
 
 import org.greenrobot.eventbus.EventBus;
-
 
 
 /**
  * Created by Administrator on 2017/7/7.
  */
 
-public class MyApplication extends Application{
+public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
